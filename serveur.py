9b118aeb7caaa34e9e562de1d67f4d0150d16908
Rhoulam Khalissa <khalissa.rhoulam@etu.univ-lyon1.fr>
2025-05-26 12:50:04 +0200

    Ebauche TCP 2

---
+++ serveur.py
@@ -0,0 +1,26 @@
+from ev3net import TCPServerAbstraction, Buffer
+import struct
+
+# Création du serveur
+server = TCPServerAbstraction(bufferSize=1024)
+server.initialize("", 9999)
+server.listenToClients()
+
+print("Serveur prêt, en attente de robots...")
+
+# Exemple : envoyer une instruction à tous les robots
+def broadcast_instruction(direction_deg, distance_cm):
+    msg = struct.pack("!ff", direction_deg, distance_cm)
+    server.broadcast(Buffer(msg))
+
+# Boucle d’exemple
+import time
+try:
+    while True:
+        # exemple d’envoi toutes les 5s
+        broadcast_instruction(45.0, 100.0)
+        print("Instruction envoyée à tous les robots.")
+        time.sleep(5)
+except KeyboardInterrupt:
+    print("Arrêt serveur.")
+    server.stop()
